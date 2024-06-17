b9af5ba634d923c9f4b00117c40e5458a0cf97fd
KrishWu <krishwu@outlook.com>
2024-06-16 18:47:51 -0700

    Fix issue for calculating invariant mass with muons if only one muon is detected properly.

--- part5/mainRootTFileTNtuple.py
+++ part5/mainRootTFileTNtuple.py
@@ -14,17 +14,18 @@ with ROOT.TFile("./part5/Tag 1 Delphes Events.root", "read") as f, ROOT.TFile("p
     hout = ROOT.TH1F("h1", "Invariant Mass Histogram;Mass;# of Particles", 100, 0, 700)
 
     for event in tree:
-        m1.SetPtEtaPhiM(event.GetLeaf("Muon", "Muon.PT").GetValue(0), event.GetLeaf("Muon", "Muon.Eta").GetValue(0), event.GetLeaf("Muon", "Muon.Phi").GetValue(0), muonMass)
-        m2.SetPtEtaPhiM(event.GetLeaf("Muon", "Muon.PT").GetValue(1), event.GetLeaf("Muon", "Muon.Eta").GetValue(1), event.GetLeaf("Muon", "Muon.Phi").GetValue(1), muonMass)
-        invariantMass = (m1 + m2).M()
+        if (event.GetLeaf("Muon", "Muon.PT").GetLen() == 2):
+            m1.SetPtEtaPhiM(event.GetLeaf("Muon", "Muon.PT").GetValue(0), event.GetLeaf("Muon", "Muon.Eta").GetValue(0), event.GetLeaf("Muon", "Muon.Phi").GetValue(0), muonMass)
+            m2.SetPtEtaPhiM(event.GetLeaf("Muon", "Muon.PT").GetValue(1), event.GetLeaf("Muon", "Muon.Eta").GetValue(1), event.GetLeaf("Muon", "Muon.Phi").GetValue(1), muonMass)
+            invariantMass = (m1 + m2).M()
 
-        #For auto scaling the x-axis.
-        if invariantMass > highestMass:
-            highestMass = invariantMass
-        elif invariantMass < lowestMass:
-            lowestMass = invariantMass
+            #For auto scaling the x-axis.
+            if invariantMass > highestMass:
+                highestMass = invariantMass
+            elif invariantMass < lowestMass:
+                lowestMass = invariantMass
 
-        hout.Fill(invariantMass)
+            hout.Fill(invariantMass)
 
     print(lowestMass, highestMass)
     #Adds the auto-scaled x-axis values to the graph.
