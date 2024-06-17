74ca980d9c42cc167f7b85a66ad44ce5b419fb98
KrishWu <krishwu@outlook.com>
2024-06-16 17:55:15 -0700

    Made some changes to allow for updated processing of the two muons that came as a result from the Delphes process for the new calculation of the invariantMass in this.

---
+++ part5/mainRootTFileTNtuple.py
@@ -0,0 +1,31 @@
+import ROOT
+import sys
+
+ROOT.gSystem.Load("~/physics/delphes-master/libDelphes.so")
+
+with ROOT.TFile("./part5/Tag 1 Delphes Events.root", "read") as f, ROOT.TFile("part5/output.root", "recreate") as outfile:
+    tree = f.Get("Delphes")
+    m1 = ROOT.TLorentzVector()
+    m2 = ROOT.TLorentzVector()
+    highestMass = 0
+    lowestMass = sys.maxsize
+    muonMass = 0.1
+    hout = ROOT.TH1F("h1", "Invariant Mass Histogram;Mass;# of Particles", 100, 0, 700)
+
+    for event in tree:
+        m1.SetPtEtaPhiM(event.GetLeaf("Muon", "Muon.PT").GetValue(0), event.GetLeaf("Muon", "Muon.Eta").GetValue(0), event.GetLeaf("Muon", "Muon.Phi").GetValue(0), muonMass)
+        m2.SetPtEtaPhiM(event.GetLeaf("Muon", "Muon.PT").GetValue(1), event.GetLeaf("Muon", "Muon.Eta").GetValue(1), event.GetLeaf("Muon", "Muon.Phi").GetValue(1), muonMass)
+        invariantMass = (m1 + m2).M()
+
+        #For auto scaling the x-axis.
+        if invariantMass > highestMass:
+            highestMass = invariantMass
+        elif invariantMass < lowestMass:
+            lowestMass = invariantMass
+
+        hout.Fill(invariantMass)
+
+    print(lowestMass, highestMass)
+    #Adds the auto-scaled x-axis values to the graph.
+    hout.GetXaxis().SetRangeUser(lowestMass, highestMass)
+    outfile.WriteObject(hout, "histogram")
