5cb6184b4adb6821acd2b41ca8faaa1213b77e72
KrishWu <krishwu@outlook.com>
2024-06-18 16:20:02 -0700

    Updated the code to properly check if the mass for multiple jets is close to the expected mass and use the one that is the closest.

--- part6/mainRootTFilepptoztojj.py
+++ part6/mainRootTFilepptoztojj.py
@@ -8,13 +8,15 @@ with ROOT.TFile("./part6/Tag 1 Delphes Events.root", "read") as f, ROOT.TFile("p
     tree = f.Get("Delphes")
     m1 = ROOT.TLorentzVector()
     m2 = ROOT.TLorentzVector()
+    massOfZBoson = 91.1876
     # highestMass = 0
     # lowestMass = sys.maxsize
     hout = ROOT.TH1F("h1", "Invariant Mass Histogram;Mass;# of Particles", 50, 0, 200)
 
     for event in tree:
         numParticles = event.GetLeaf("Jet", "Jet.PT").GetLen()
-        deltaM = sys.maxsize
+        # print("numParticles", numParticles)
+        invariantMass = 0
 
         if (numParticles < 2):
             continue
@@ -22,17 +24,21 @@ with ROOT.TFile("./part6/Tag 1 Delphes Events.root", "read") as f, ROOT.TFile("p
         for i in range(numParticles - 1):
             for j in range (numParticles - 1):
                 if (j >= i):
-                    if (deltaM > abs(event.GetLeaf("Jet", "Jet.Charge").GetValue(i) - (-1 * event.GetLeaf("Jet", "Jet.Charge").GetValue(j + 1)))):
-                        deltaM = abs(event.GetLeaf("Jet", "Jet.Charge").GetValue(i) - (-1 * event.GetLeaf("Jet", "Jet.Charge").GetValue(j + 1)))
-                        m1.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(i), event.GetLeaf("Jet", "Jet.Eta").GetValue(i), event.GetLeaf("Jet", "Jet.Phi").GetValue(i), event.GetLeaf("Jet", "Jet.Charge").GetValue(i))
-                        m2.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(j + 1), event.GetLeaf("Jet", "Jet.Eta").GetValue(j + 1), event.GetLeaf("Jet", "Jet.Phi").GetValue(j + 1), event.GetLeaf("Jet", "Jet.Charge").GetValue(j + 1))
+                    m1.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(i), event.GetLeaf("Jet", "Jet.Eta").GetValue(i), event.GetLeaf("Jet", "Jet.Phi").GetValue(i), event.GetLeaf("Jet", "Jet.Charge").GetValue(i))
+                    m2.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(j + 1), event.GetLeaf("Jet", "Jet.Eta").GetValue(j + 1), event.GetLeaf("Jet", "Jet.Phi").GetValue(j + 1), event.GetLeaf("Jet", "Jet.Charge").GetValue(j + 1))
+                    # print(i, j + 1)
                 else:
-                    if (deltaM > abs(event.GetLeaf("Jet", "Jet.Charge").GetValue(i) - (-1 * event.GetLeaf("Jet", "Jet.Charge").GetValue(j)))):
-                        deltaM = event.GetLeaf("Jet", "Jet.Charge").GetValue(i) - (-1 * event.GetLeaf("Jet", "Jet.Charge").GetValue(j))
-                        m1.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(i), event.GetLeaf("Jet", "Jet.Eta").GetValue(i), event.GetLeaf("Jet", "Jet.Phi").GetValue(i), event.GetLeaf("Jet", "Jet.Charge").GetValue(i))
-                        m2.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(j), event.GetLeaf("Jet", "Jet.Eta").GetValue(j), event.GetLeaf("Jet", "Jet.Phi").GetValue(j), event.GetLeaf("Jet", "Jet.Charge").GetValue(j))
+                    m1.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(i), event.GetLeaf("Jet", "Jet.Eta").GetValue(i), event.GetLeaf("Jet", "Jet.Phi").GetValue(i), event.GetLeaf("Jet", "Jet.Charge").GetValue(i))
+                    m2.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(j), event.GetLeaf("Jet", "Jet.Eta").GetValue(j), event.GetLeaf("Jet", "Jet.Phi").GetValue(j), event.GetLeaf("Jet", "Jet.Charge").GetValue(j))
+                    # print(i, j)
+                tempInvariantMass = (m1+m2).M()
+                # print("temp", tempInvariantMass, "actual", invariantMass)
+                # print(abs(tempInvariantMass - massOfZBoson) < abs(invariantMass - massOfZBoson))
+                if (abs(tempInvariantMass - massOfZBoson) < abs(invariantMass - massOfZBoson)):
+                    invariantMass = tempInvariantMass
+                    # print("curr invariantMass", invariantMass)
 
-        invariantMass = (m1+m2).M()
+        # print("Final invariant Mass", invariantMass)
 
         # #For auto scaling the x-axis.
         # if invariantMass > highestMass:
@@ -41,6 +47,7 @@ with ROOT.TFile("./part6/Tag 1 Delphes Events.root", "read") as f, ROOT.TFile("p
         #     lowestMass = invariantMass
 
         hout.Fill(invariantMass)
+        # print()
 
     # print(lowestMass, highestMass)
     # #Adds the auto-scaled x-axis values to the graph.
