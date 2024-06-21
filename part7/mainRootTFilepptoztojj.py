1b86ff60ce3e976d8a1ca58e261570493710a2e9
KrishWu <krishwu@outlook.com>
2024-06-20 18:56:31 -0700

    Made it properly display to the screen.

--- part7/mainRootTFilepptoztojj.py
+++ part7/mainRootTFilepptoztojj.py
@@ -1,5 +1,6 @@
 import ROOT
 import sys
+import math
 
 ROOT.gInterpreter.AddIncludePath("~/physics/delphes-master/external/")
 ROOT.gSystem.Load("~/physics/delphes-master/libDelphes.so")
@@ -15,12 +16,15 @@ with ROOT.TFile("./part7/tag_1_delphes_events.root", "read") as f, ROOT.TFile("p
     # lowestMass = sys.maxsize
     houtWBoson = ROOT.TH1F("h1", "Invariant Mass Histogram W Boson;Mass;# of Particles", 50, 0, 200)
     houtTParticle = ROOT.TH1F("h2", "Invariant Mass Histogram T Particle;Mass;# of Particles", 100, 0, 500)
+    c1 = ROOT.TCanvas("c1", "Invariant Mass Histogram W Boson", 700, 500)
+    c2 = ROOT.TCanvas("c2", "Invariant Mass Histogram T Particle", 700, 500)
 
-    for event in tree:
+    for event in tree: 
         numParticles = event.GetLeaf("Jet", "Jet.PT").GetLen()
         wBoson = m1
         tParticle = m3
-        numBJets = 0
+        numBJetsPositiveCharged = 0
+        numBJetsNegativeCharged = 0
         numNonBJets = 0
 
         #Check if not enough particles.
@@ -30,12 +34,15 @@ with ROOT.TFile("./part7/tag_1_delphes_events.root", "read") as f, ROOT.TFile("p
         #Count how many of each type of particle B or notB.
         for i in range(numParticles):
             if (event.GetLeaf("Jet", "Jet.BTag").GetValue(i) == 1):
-                numBJets += 1
+                if (event.GetLeaf("Jet", "Jet.Charge").GetValue(i) > 0):
+                    numBJetsPositiveCharged += 1
+                else:
+                    numBJetsNegativeCharged += 1
             else:
                 numNonBJets += 1
         
-        #Check if there is enough of each type of particle B or notB.
-        if (numBJets < 1 or numNonBJets < 2):
+        #Check if there is enough of each type of particle notB.
+        if (numNonBJets < 2):
             continue
 
         for i in range(numParticles - 1):
@@ -54,10 +61,16 @@ with ROOT.TFile("./part7/tag_1_delphes_events.root", "read") as f, ROOT.TFile("p
                 tempWBoson = m1+m2
                 if (abs(tempWBoson.M() - massOfWBoson) < abs(wBoson.M() - massOfWBoson)):
                     wBoson = tempWBoson
+
+        houtWBoson.Fill(wBoson.M())
+        
+        #Check if there is enough of each type of particle B.
+        if (numBJetsNegativeCharged < 1):
+            continue
         
         for i in range(numParticles):
             #Check if either particle is not a B particle and if so continue.
-            if (event.GetLeaf("Jet", "Jet.BTag").GetValue(i) == 0):
+            if (event.GetLeaf("Jet", "Jet.BTag").GetValue(i) == 0 or event.GetLeaf("Jet", "Jet.Charge").GetValue(i) > 0):
                 continue
             #Calculate the mass of the T Particle and check if it is closer.
             m3.SetPtEtaPhiM(event.GetLeaf("Jet", "Jet.PT").GetValue(i), event.GetLeaf("Jet", "Jet.Eta").GetValue(i), event.GetLeaf("Jet", "Jet.Phi").GetValue(i), event.GetLeaf("Jet", "Jet.Mass").GetValue(i))
@@ -70,12 +83,33 @@ with ROOT.TFile("./part7/tag_1_delphes_events.root", "read") as f, ROOT.TFile("p
         #     highestMass = invariantMass
         # elif invariantMass < lowestMass:
         #     lowestMass = invariantMass
-
-        houtWBoson.Fill(wBoson.M())
+        
         houtTParticle.Fill(tParticle.M())
+
+        # if (tParticle.M() < 50):
+        #     print("Speciallll T")
+        
+        # if (wBoson.M() < 30):
+        #     print("Speciallll W")
+        
+        # for i in range(numParticles):
+        #     if (event.GetLeaf("Jet", "Jet.BTag").GetValue(i) == 0):
+        #         print("Particle ", i, " Momentum: ", event.GetLeaf("Jet", "Jet.PT").GetValue(i) * math.cosh(event.GetLeaf("Jet", "Jet.Eta").GetValue(i)), " Mass: ", event.GetLeaf("Jet", "Jet.Mass").GetValue(i), " Charge: ", event.GetLeaf("Jet", "Jet.Charge").GetValue(i), " BTag: ", event.GetLeaf("Jet", "Jet.BTag").GetValue(i))
+        # for i in range(numParticles):
+        #     if (event.GetLeaf("Jet", "Jet.BTag").GetValue(i) == 1):
+        #         print("Particle ", i, " Momentum: ", event.GetLeaf("Jet", "Jet.PT").GetValue(i) * math.cosh(event.GetLeaf("Jet", "Jet.Eta").GetValue(i)), " Mass: ", event.GetLeaf("Jet", "Jet.Mass").GetValue(i), " Charge: ", event.GetLeaf("Jet", "Jet.Charge").GetValue(i), " BTag: ", event.GetLeaf("Jet", "Jet.BTag").GetValue(i))
+        # print("W Boson: ", wBoson.M(), " T Particle: ", tParticle.M())
+        # print()
 
     # print(lowestMass, highestMass)
     # #Adds the auto-scaled x-axis values to the graph.
     # hout.GetXaxis().SetRangeUser(lowestMass, highestMass)
     outfile.WriteObject(houtWBoson, "histogram")
     outfile.WriteObject(houtTParticle, "histogram")
+    c1.cd()
+    houtWBoson.Draw()
+    c2.cd()
+    houtTParticle.Draw()
+    c1.Update()
+    c2.Update()
+    ROOT.gApplication.Run()
